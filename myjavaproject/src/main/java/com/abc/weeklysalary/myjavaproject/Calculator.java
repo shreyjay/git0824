9ea33c29a1b42daa2d578b5ffc0161ac15087a6b
shreyjay <shreya.kumbhani@yahoo.com>
2024-08-24 12:15:11 -0500

    upload first project

---
+++ myjavaproject/src/main/java/com/abc/weeklysalary/myjavaproject/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.abc.weeklysalary.myjavaproject;
+
+/**
+ *
+ * @author hemap
+ */
+public class Calculator {
+    
+    public int add(int x, int y) {
+        return x+y;
+    }
+    
+    public int mul(int x, int y) {
+        return x*y;
+    }
+    
+}
